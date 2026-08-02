def095b8c85972492b7bebbd899f44841396ca45
Adam Dybcio <adam.dybcio.kontakt@gmail.com>
2026-08-02 15:36:15 +0200

    init

---
+++ src/main/java/pl/dybcio/ordered_backend/OrderedBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.dybcio.ordered_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dybcio/ordered_backend/OrderedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dybcio.ordered_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
