b6e74bb9146f240ffca2ff6ccc0bcb0672cf3b1e
Adam Dybcio <adam.dybcio.kontakt@gmail.com>
2026-08-03 22:50:51 +0200

    Add CI configuration and improve code formatting and structure

--- src/main/java/pl/dybcio/ordered/catalog/repository/ProductRepository.java
+++ src/main/java/pl/dybcio/ordered/catalog/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package pl.dybcio.ordered.catalog.repository;
 
-import pl.dybcio.ordered.catalog.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.dybcio.ordered.catalog.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
